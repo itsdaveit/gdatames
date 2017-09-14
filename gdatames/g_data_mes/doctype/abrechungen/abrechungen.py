0bd692305aaab27d10b0876390fa0de2f214b58f
David Malinowski <dave@dave-mbp.itsdave.local>
2017-09-14 22:32:48 +0200

    WIP

--- gdatames/g_data_mes/doctype/abrechungen/abrechungen.py
+++ gdatames/g_data_mes/doctype/abrechungen/abrechungen.py
@@ -9,6 +9,7 @@ import zipfile
 from frappe.utils import file_manager
 import fnmatch
 import xml.etree.ElementTree as ET
+import re
 
 
 class Abrechungen(Document):
@@ -20,13 +21,19 @@ class Abrechungen(Document):
 		mes_xml = zip_xml_content[0]
 		self.xml_data = mes_xml
 		xml_et = ET.fromstring(self.xml_data)
+		date = re.search('(?<=private/files/)(\d\d)_(\d\d\d\d)_mes_usage_export.zip', gdata_zipfile)
+		invoice_month = date.group(1)
+		invoice_year = date.group(2)
 		reportentrys = xml_et.findall('ReportEntry')
 		#Momentan ist die Verarbeitung auf einen Report pro XML Datei begrenzt.
 		if len(reportentrys) == 1:
+			#if self.xml_data != '':
+			#	frappe.utils.file_manager.remove_file(attached_file[0].name)
 			log = 'Beginne mit der Verarbeitung der Reports\n'
 			for reportentry in reportentrys:
 				counter_MaxActiveClients = 0
 				log = log + 'Report für Firma: ' + reportentry.attrib['Company'] + '\n'
+				log = log + 'Abrechnungsmonat: ' + invoice_month + '.' + invoice_year + '\n'
 				log = log + 'G Data Kundenummer: ' + reportentry.attrib['GDCustomerNr'] + '\n'
 				log = log + 'Login Name: ' + reportentry.attrib['Login'] + '\n'
 				log = log + 'Produkt: ' + reportentry.attrib['Product'] + '\n'
@@ -35,14 +42,42 @@ class Abrechungen(Document):
 				for managementserver in managementservers:
 					log = log + 'Server mit ID: ' + managementserver.attrib['id'] + ' mit ' + managementserver.attrib['MaxActiveClients'] + ' aktiven Clients gefunden.\n'
 					counter_MaxActiveClients = counter_MaxActiveClients + int(managementserver.attrib['MaxActiveClients'])
+					self.create_mes_invoice(managementserver.attrib['id'], managementserver.attrib['MaxActiveClients'], invoice_month, invoice_year )
 				log = log + 'Gesamtanzahl Clients gezählt: ' + str(counter_MaxActiveClients)
-				self.log = log
-
-
+			self.log = log
 		else:
 			msgprint('Keinen oder mehr als ein ReportEntry im XML-Code gefunden, breche ab.')
 
 
+	def create_mes_invoice(self, mes_id, max_active_clients, invoice_month, invoice_year):
+		management_server = frappe.get_all('Management Server', {"management_server_id": mes_id})
+		if len(management_server) == 0:
+			frappe.throw('Management Server ID ' + mes_id + ' nicht gefunden.')
+		if len(management_server) == 1:
+			doc_management_server = frappe.get_doc("Management Server", management_server[0].name)
+			product = frappe.get_doc("Produkte", doc_management_server.product)
+			item = frappe.get_doc("Item", product.item)
+			sales_invoice_item = frappe.get_doc({"doctype": "Sales Invoice Item",
+									"item_code": item.name,
+									"qty": int(max_active_clients),
+									#"item_name": item.name,
+									#"conversion_factor": 1,
+									#"uom": item.stock_uom,
+									#"description": item.description,
+									#"income_account": item.income_account
+									})
+
+			print item.name
+			sales_invoice_doc = frappe.get_doc({"doctype": "Sales Invoice",
+									"title": "G Data MES Abrechung " + invoice_month + "." + invoice_year,
+									"customer": doc_management_server.customer,
+									#"against_income_account" : "Vertrieb - IG",
+									"status": "Draft"})
+
+			sales_invoice_doc.append("items", sales_invoice_item)
+			sales_invoice_doc.insert()
+		else:
+			frappe.throw('Management Server ID ' + mes_id + ' nich einmalig.')
 
 
 	def extract_xml_from_zip(self, inputzip):
